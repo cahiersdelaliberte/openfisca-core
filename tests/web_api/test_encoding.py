78d6a3e84d7d5a1c5990b8ad10eb8654f07b2d2a
sandcha <sandcha@users.noreply.github.com>
2017-12-05 11:29:32 +0100

    fixup! Check calculate response encoding

--- tests/web_api/test_encoding.py
+++ tests/web_api/test_encoding.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from httplib import BAD_REQUEST, OK, NOT_FOUND
+from httplib import BAD_REQUEST, OK
 import json
-import dpath
-from nose.tools import assert_equal, assert_in
+import os
+from nose.tools import assert_equal
 from . import subject
 import logging
 
@@ -17,15 +17,6 @@ def post_json(data = None, file = None):
         with open(file_path, 'r') as file:
             data = file.read()
     return subject.post('/calculate', data = data, content_type = 'application/json')
-
-
-def check_response(data, expected_error_code, path_to_check, content_to_check):
-    response = post_json(data)
-    assert_equal(response.status_code, expected_error_code)
-    json_response = json.loads(response.data, encoding='utf-8')
-    if path_to_check:
-        content = dpath.util.get(json_response, path_to_check)
-        assert_in(content_to_check, content)
 
 
 def test_encoding():
@@ -49,8 +40,8 @@ def test_encoding():
         }
     })
 
+    # No UnicodeDecodeError
     expected_response = "'Locataire ou sous-locataire d‘un logement loué vide non-HLM' is not a valid value for 'housing_occupancy_status'. Possible values are ['Tenant', 'Owner', 'Free logder', 'Homeless']."
-    check_response(simulation_json, BAD_REQUEST, 'households/_/housing_occupancy_status/2017-07', expected_response)
     response = post_json(simulation_json)
-    assertresponse.data.decode('utf-8')
-    assert_equal(response.status_code, OK, response.data)
+    assert expected_response in response.data, expected_response + os.linesep + " NOT FOUND IN: " + os.linesep + response.data
+    assert_equal(response.status_code, BAD_REQUEST, response.data)
