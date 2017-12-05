68d5523358e613b818fc0c9c2027ef8f937e30e4
sandcha <sandcha@users.noreply.github.com>
2017-12-05 10:12:55 +0100

    Check calculate response encoding

--- tests/web_api/test_encoding.py
+++ tests/web_api/test_encoding.py
@@ -2,7 +2,8 @@
 
 from httplib import BAD_REQUEST, OK, NOT_FOUND
 import json
-from nose.tools import assert_equal
+import dpath
+from nose.tools import assert_equal, assert_in
 from . import subject
 import logging
 
@@ -18,10 +19,20 @@ def post_json(data = None, file = None):
     return subject.post('/calculate', data = data, content_type = 'application/json')
 
 
+def check_response(data, expected_error_code, path_to_check, content_to_check):
+    response = post_json(data)
+    assert_equal(response.status_code, expected_error_code)
+    json_response = json.loads(response.data, encoding='utf-8')
+    if path_to_check:
+        content = dpath.util.get(json_response, path_to_check)
+        assert_in(content_to_check, content)
+
+
 def test_encoding():
     simulation_json = json.dumps({
         "persons": {
-            "bob": {}
+            "O‘Ryan": {},
+            "Renée": {}
         },
         "households": {
             "_": {
@@ -30,14 +41,16 @@ def test_encoding():
     
                 },
                 "parent": [
-                    "bob"
+                    "O‘Ryan",
+                    "Renée"
                 ],
                 "children": [ ]
             }
         }
     })
 
+    expected_response = "'Locataire ou sous-locataire d‘un logement loué vide non-HLM' is not a valid value for 'housing_occupancy_status'. Possible values are ['Tenant', 'Owner', 'Free logder', 'Homeless']."
+    check_response(simulation_json, BAD_REQUEST, 'households/_/housing_occupancy_status/2017-07', expected_response)
     response = post_json(simulation_json)
-    log.info(response.data)
-    # json_response = json.loads(response.data, encoding='utf-8')
+    assertresponse.data.decode('utf-8')
     assert_equal(response.status_code, OK, response.data)
